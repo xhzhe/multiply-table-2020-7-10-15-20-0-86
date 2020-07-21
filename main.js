0f38ea666e15a80080294cf069c373088057da03
ALEX H Z XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUAL7@OOCL.COM>
2020-07-21 16:12:17 +0800

    init

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
 function createMultiplyTable(start, end) {
-  return null;
+    if (isValid(start, end)) {
+        let res = "";
+        for (let i = start; i <= end; i++) {
+            res += getLine(start, i);
+            if (i != end) {
+                res += "\n";
+            }
+        }
+        return res;
+    }
+    return null;
+}
+function getLine(start, end) {
+    let ans = [];
+    for (let i = start; i <= end; i++) {
+        ans.push(getExpression(i, end));
+    }
+    return ans.join("\t");
+}
+
+function getExpression(multiplier, multiplicand) {
+    return multiplier + "*" + multiplicand + "=" + multiplicand * multiplier;
+}
+
+function isValid(begin, end) {
+    if (end < begin) {
+        return false;
+    }
+    if (begin >= 1 && begin <= 1000 && end >= 1 && end <= 1000) {
+        return true;
+    }
+    return false;
 }
 
 module.exports = {
-  createMultiplyTable,
+    createMultiplyTable,
 };
